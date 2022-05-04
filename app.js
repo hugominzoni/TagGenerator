0be7b49dd3a3dbea18ca9897146c979a14cff7eb
Hugo Minzoni <hugominzoni@gmail.com>
2022-05-04 19:10:10 -0400

    selected class added

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const itemArr = []
 
 for(let i = 0; i< item.length; i++){
     item[i].addEventListener('click', function(){
-        item[i].style.color = "green"
-        item[i].style.fontWeight = "bold"
+        item[i].classList.add('selected')
         itemArr.push(item[i].innerHTML)
         const printArr = document.querySelector('h3').innerHTML = (itemArr.join(' '))
         const btn = document.querySelector('.btn')

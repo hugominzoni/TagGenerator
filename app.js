4c25f1e0bcb1d1ecc42ff55eea4ca0f1bd88b926
Hugo Minzoni <50162408+hugominzoni@users.noreply.github.com>
2022-05-04 16:43:15 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const item = document.querySelectorAll('.item')
+const itemArr = []
+
+
+
+for(let i = 0; i< item.length; i++){
+    item[i].addEventListener('click', function(){
+        item[i].style.color = "green"
+        item[i].style.fontWeight = "bold"
+        itemArr.push(item[i].innerHTML)
+        const printArr = document.querySelector('h3').innerHTML = (itemArr.join(' '))
+        const btn = document.querySelector('.btn')
+        btn.addEventListener('click', (event) =>{
+            const copied = document.querySelector('h3').textContent
+            navigator.clipboard.writeText(copied)
+            const info = document.querySelector('p')
+            info.classList.remove("hidden")        
+            info.classList.add("hidden")        
+            info.classList.toggle("hidden")        
+        })
+            
+        
+    })
+}
+
+
